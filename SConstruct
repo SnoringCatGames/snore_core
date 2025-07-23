645916a831975dfeb287c9451c1108b8af4f2fd8
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-07-23 14:30:17 -0700

    Continue restructuring submodules.

--- SConstruct
+++ SConstruct
@@ -5,14 +5,33 @@ import sys
 from build_utils import post_setup, pre_setup, set_up
 
 
-lib_name = "SnoreCore"
-addon_dir_name = "snore_core"
+snore_core_lib_name = "SnoreCore"
+snore_core_addon_dir_name = "snore_core"
 
 env = pre_setup(ARGUMENTS, Environment, Variables, Help, SConscript)
 
 cpp_paths = []
 sources = []
+libs = []
+lib_paths = []
 
-set_up(env, cpp_paths, sources, addon_dir_name, is_setup_for_self=True)
+set_up(
+    env,
+    cpp_paths,
+    sources,
+    libs,
+    lib_paths,
+    snore_core_addon_dir_name,
+    is_setup_for_self=True,
+)
 
-post_setup(env, cpp_paths, sources, lib_name, addon_dir_name, Default)
+post_setup(
+    env,
+    cpp_paths,
+    sources,
+    libs,
+    lib_paths,
+    snore_core_lib_name,
+    snore_core_addon_dir_name,
+    Default,
+)
--- build_utils.py
+++ build_utils.py
@@ -4,6 +4,10 @@ import sys
 from enum import Enum
 
 
+default_lib_name = "SnoreCore"
+default_addon_dir_name = "snore_core"
+
+
 # Colors are disabled in non-TTY environments such as pipes. This means
 # that if output is redirected to a file, it won't contain color codes.
 # Colors are always enabled on continuous integration.
@@ -46,15 +50,31 @@ class ANSI(Enum):
 
 def print_warning(*values: object) -> None:
     """Prints a warning message with formatting."""
-    print(f"{ANSI.YELLOW}{ANSI.BOLD}WARNING:{ANSI.REGULAR}", *values, ANSI.RESET, file=sys.stderr)
+    print(
+        f"{ANSI.YELLOW}{ANSI.BOLD}WARNING:{ANSI.REGULAR}",
+        *values,
+        ANSI.RESET,
+        file=sys.stderr,
+    )
 
 
 def print_error(*values: object) -> None:
     """Prints an error message with formatting."""
-    print(f"{ANSI.RED}{ANSI.BOLD}ERROR:{ANSI.REGULAR}", *values, ANSI.RESET, file=sys.stderr)
+    print(
+        f"{ANSI.RED}{ANSI.BOLD}ERROR:{ANSI.REGULAR}",
+        *values,
+        ANSI.RESET,
+        file=sys.stderr,
+    )
 
 
-def pre_setup(ARGUMENTS: dict, Environment: object, Variables: object, Help: object, SConscript: object) -> object:
+def pre_setup(
+    ARGUMENTS: dict,
+    Environment: object,
+    Variables: object,
+    Help: object,
+    SConscript: object,
+) -> object:
     localEnv = Environment(tools=["default"], PLATFORM="")
 
     # Build profiles can be used to decrease compile times.
@@ -65,7 +85,7 @@ def pre_setup(ARGUMENTS: dict, Environment: object, Variables: object, Help: obj
 
     # localEnv["build_profile"] = "build_profile.json"
 
-    customs = ['custom.py']
+    customs = ["custom.py"]
     customs = [os.path.abspath(path) for path in customs]
 
     opts = Variables(customs, ARGUMENTS)
@@ -75,29 +95,31 @@ def pre_setup(ARGUMENTS: dict, Environment: object, Variables: object, Help: obj
 
     env = localEnv.Clone()
 
-    if not os.path.isdir('googletest'):
+    if not os.path.isdir("googletest"):
         print_error("googletest must be a submodule of the root repository.")
         sys.exit(1)
-    if not os.path.isdir('godot-cpp'):
+    if not os.path.isdir("godot-cpp"):
         print_error("godot-cpp must be a submodule of the root repository.")
         sys.exit(1)
     if not (os.path.isdir("godot-cpp") and os.listdir("godot-cpp")):
-        print_error("""godot-cpp is not available within this folder, as Git submodules haven't been initialized.
+        print_error(
+            """godot-cpp is not available within this folder, as Git submodules haven't been initialized.
     Run the following command to download godot-cpp:
 
-        git submodule update --init --recursive""")
+        git submodule update --init --recursive"""
+        )
         sys.exit(1)
 
     env = SConscript("godot-cpp/SConstruct", {"env": env, "customs": customs})
 
-    env['is_debug_build'] = ARGUMENTS.get("target", "") in ["editor", "template_debug"]
-    env['is_continuous_integration'] = ARGUMENTS.get("ci", "") == "yes"
-    env['includes_tests'] = ARGUMENTS.get("tests", "") == "yes"
+    env["is_debug_build"] = ARGUMENTS.get("target", "") in ["editor", "template_debug"]
+    env["is_continuous_integration"] = ARGUMENTS.get("ci", "") == "yes"
+    env["includes_tests"] = ARGUMENTS.get("tests", "") == "yes"
 
-    if env['is_continuous_integration']:
+    if env["is_continuous_integration"]:
         env.Append(CPPDEFINES=["SC_CI_ENABLED"])
 
-    if env['includes_tests']:
+    if env["includes_tests"]:
         env.Append(CPPDEFINES=["SC_TESTS_ENABLED"])
 
     # Enable C++23.
@@ -112,65 +134,86 @@ def pre_setup(ARGUMENTS: dict, Environment: object, Variables: object, Help: obj
     return env
 
 
-def post_setup(env: object, cpp_paths: list[str], sources: list[str], lib_name: str, addon_dir_name: str, Default: object) -> None:
-    if env['is_debug_build']:
+def post_setup(
+    env: object,
+    cpp_paths: list[str],
+    sources: list[str],
+    libs: list[str],
+    lib_paths: list[str],
+    lib_name: str,
+    addon_dir_name: str,
+    Default: object,
+) -> None:
+    if env["is_debug_build"]:
         try:
-            doc_data = env.GodotCPPDocData("src/gen/doc_data.gen.cpp", source=glob.glob("doc_classes/*.xml"))
+            doc_data = env.GodotCPPDocData(
+                "src/gen/doc_data.gen.cpp", source=glob.glob("doc_classes/*.xml")
+            )
             sources.append(doc_data)
         except AttributeError:
-            print("Not including class reference as we're targeting a pre-4.3 baseline.")
-    
+            print(
+                "Not including class reference as we're targeting a pre-4.3 baseline."
+            )
+
     # Filter-out generated files.
     sources_copy = sources.copy()
     sources = [f for f in sources_copy if not str(f).endswith(".gen.cpp")]
 
     env.Append(CPPPATH=cpp_paths)
+    env.Append(LIBS=libs)
+    env.Append(LIBPATH=lib_paths)
 
-    # .dev doesn't inhibit compatibility, so we don't need to key it.
-    # .universal just means "compatible with all relevant arches" so we don't need to key it.
-    suffix = env['suffix'].replace(".dev", "").replace(".universal", "")
-
-    lib_filename = "{}{}{}{}".format(env.subst('$SHLIBPREFIX'), lib_name, suffix, env.subst('$SHLIBSUFFIX'))
-
+    lib_filename = create_lib_filename(env, lib_name)
     library = env.SharedLibrary(
-        "bin/{}/{}".format(env['platform'], lib_filename),
+        "bin/{}/{}".format(env["platform"], lib_filename),
         source=sources,
     )
 
-    copy = env.Install("demo/addons/{}/bin/{}/".format(addon_dir_name, env["platform"]), library)
+    addon_platform_dir_name = create_addon_platform_dir_name(env, addon_dir_name)
+    copy = env.Install(addon_platform_dir_name, library)
 
     default_args = [library, copy]
     Default(*default_args)
 
 
-def set_up(env: object, cpp_paths: list[str], sources: list[str], snore_core_addon_dir_name: str, is_setup_for_self = False) -> None:
+def set_up(
+    env: object,
+    cpp_paths: list[str],
+    sources: list[str],
+    libs: list[str],
+    lib_paths: list[str],
+    snore_core_addon_dir_name: str,
+    is_setup_for_self=False,
+) -> None:
     if is_setup_for_self:
-        cpp_paths.extend([
-            "src/",
-        ])
-        sources.extend(
-            glob.glob("src/**/*.cpp", recursive=True)
-        )
+        cpp_paths.extend(["src/"])
+        sources.extend(glob.glob("src/**/*.cpp", recursive=True))
     else:
-        cpp_paths.extend([
-            snore_core_addon_dir_name + "/src/",
-        ])
-        sources.extend(
-            glob.glob(snore_core_addon_dir_name + "/src/snore_core/**/*.cpp", recursive=True)
+        cpp_paths.extend([snore_core_addon_dir_name + "/src/"])
+        # Use a DLL rather than statically including .cpp files.
+        # sources.extend([])
+        lib_filename = create_lib_filename(env, default_lib_name)
+        libs.extend([lib_filename])
+        addon_platform_dir_name = create_addon_platform_dir_name(
+            env, snore_core_addon_dir_name
+        )
+        lib_paths.extend([addon_platform_dir_name])
+
+    if env["includes_tests"]:
+        cpp_paths.extend(
+            [
+                "googletest/googletest/",
+                "googletest/googletest/include/",
+                "googletest/googlemock/",
+                "googletest/googlemock/include/",
+            ]
         )
 
-    if env['includes_tests']:
-        cpp_paths.extend([
-            "googletest/googletest/",
-            "googletest/googletest/include/",
-            "googletest/googlemock/",
-            "googletest/googlemock/include/",
-        ])
-
-        googletest_sources = ([
-            "googletest/googletest/src/gtest-all.cc",
-            "googletest/googlemock/src/gmock-all.cc",
-        ]
+        googletest_sources = (
+            [
+                "googletest/googletest/src/gtest-all.cc",
+                "googletest/googlemock/src/gmock-all.cc",
+            ]
             # glob.glob("googletest/googletest/src/*.cc") +
             # glob.glob("googletest/googlemock/src/*.cc")
         )
@@ -180,4 +223,29 @@ def set_up(env: object, cpp_paths: list[str], sources: list[str], snore_core_add
             # "googletest/googlemock/src/gmock-all.cc",
             # "googletest/googlemock/src/gmock_main.cc",
         ]
-        sources.extend([x for x in googletest_sources if str(x) not in googletest_exclusions])
+        sources.extend(
+            [x for x in googletest_sources if str(x) not in googletest_exclusions]
+        )
+
+
+def create_symlink() -> None:
+    """
+    Make the SnoreCore GDExtension and GDScript addon files accessible from the root module's demo.
+    """
+    original_path = os.path.abspath("snore_core/demo/addons/snore_core")
+    link_path = os.path.abspath("demo/addons/snore_core")
+    if not os.path.lexists(link_path):
+        os.symlink(original_path, link_path, target_is_directory=True)
+
+
+def create_lib_filename(env: object, lib_name: str) -> str:
+    # .dev doesn't inhibit compatibility, so we don't need to key it.
+    # .universal just means "compatible with all relevant arches" so we don't need to key it.
+    suffix = env["suffix"].replace(".dev", "").replace(".universal", "")
+    return "{}{}{}{}".format(
+        env.subst("$SHLIBPREFIX"), lib_name, suffix, env.subst("$SHLIBSUFFIX")
+    )
+
+
+def create_addon_platform_dir_name(env: object, addon_dir_name: str) -> str:
+    return "demo/addons/{}/bin/{}/".format(addon_dir_name, env["platform"])
